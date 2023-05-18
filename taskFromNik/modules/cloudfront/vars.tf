e383f977cb5be700b200bde15f3a03a1e9dcdde5
Stefan Baev <79969033+stefobaev@users.noreply.github.com>
2023-05-18 17:11:11 +0300

    Update vars.tf

--- taskFromNik/modules/cloudfront/vars.tf
+++ taskFromNik/modules/cloudfront/vars.tf
@@ -2,11 +2,11 @@ variable "bucketName" {}
 variable "bucket_name" {}
 variable "bucket_arn" {}
 variable "certificateArn" {
-    default = "arn:aws:acm:us-east-1:089370973671:certificate/958fcc72-7152-452c-9eab-0fc4af91856f"
+    default = "your-certificate-arn"
 }
 
 variable "cloudFrontComment" {
-    default = "Baev Society CloudFront distribution"
+    default = " Society CloudFront distribution"
 }
 
 variable "cloudFrontDefaultRootObject" {
@@ -18,5 +18,5 @@ variable "cloudFrontCertificateVersion" {
 }
 
 variable "hostedZoneId" {
-    default = "Z0637605ZR276D7FVLBB"
+    default = "your-hosted-zone"
 }
